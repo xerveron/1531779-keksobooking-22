2dc76b6a87d57b9e50da41831497ed227da0b586
xerveron <xerveron@gmail.com>
2021-02-22 21:31:23 +0300

    Модуляция

--- js/util.js
+++ js/util.js
@@ -1,14 +1,14 @@
-const getRandomForReduce = (a,b) => {
-    if (getRandomNumber (0,1)) a.push(b);
-    return a;
-  };
+const getRandomForReduce = (a, b) => {
+  if (getRandomNumber(0, 1)) a.push(b);
+  return a;
+};
 
-  const getRandomNumber = (from, to) => {
-    return (from===to) ? 'Числа не должны быть одинаковыми': (from>=to) ? 'Первое число должно быть меньше второго' : Math.round(Math.random()*(to-from)+from);
-  };
-  
-  const getRandomFloatNumber = (from, to, decimals) => {
-    return (from===to) ? 'Числа не должны быть одинаковыми': (from>=to) ? 'Первое число должно быть меньше второго' : Math.round(((Math.random()*(to-from))+from)*Math.pow(10,decimals))/Math.pow(10,decimals);
-  };
+const getRandomNumber = (from, to) => {
+  return (from === to) ? 'Числа не должны быть одинаковыми' : (from >= to) ? 'Первое число должно быть меньше второго' : Math.round(Math.random() * (to - from) + from);
+};
 
-export {getRandomFloatNumber, getRandomNumber,getRandomForReduce};
+const getRandomFloatNumber = (from, to, decimals) => {
+  return (from === to) ? 'Числа не должны быть одинаковыми' : (from >= to) ? 'Первое число должно быть меньше второго' : Math.round(((Math.random() * (to - from)) + from) * Math.pow(10, decimals)) / Math.pow(10, decimals);
+};
+
+export { getRandomFloatNumber, getRandomNumber, getRandomForReduce };
