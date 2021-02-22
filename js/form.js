f9ba72779053f4d9626cd6bb958950059e80e3bf
xerveron <xerveron@gmail.com>
2021-02-23 01:04:44 +0300

    Отрисуй еня полностью часть 2

---
+++ js/form.js
@@ -0,0 +1,29 @@
+import {dropDownChange} from './util.js';
+
+
+const changePriceOfType = (select, input) => {
+    select.addEventListener('change', () => {
+    if (select.value === 'bungalow') {
+      dropDownChange(input, '2000');
+    } else if (select.value === 'flat') {
+      dropDownChange(input, '5000');
+    } else if (select.value === 'house') {
+      dropDownChange(input, '10000');
+    } else {
+      dropDownChange(input, '50000');
+    }
+  });
+};
+
+const eventChange = (a,b) => {
+  a.addEventListener ('change', () => {
+    b.value = a.value;
+  });};
+
+const eventBothChange = (a, b) => {  
+  eventChange(a,b);
+  eventChange (b,a);
+};
+
+export {changePriceOfType, eventBothChange};
+
