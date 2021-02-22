9437ad9514685f773689209d84ad518df04fecb9
xerveron <xerveron@gmail.com>
2021-02-23 01:04:44 +0300

    Отрисуй еня полностью часть 2

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 
 import {offerPopUp} from './offer.js';
+import {changePriceOfType, eventBothChange} from './form.js';
 
 const offerList = document.querySelector('#map-canvas');
 
@@ -7,3 +8,14 @@ const fragment = document.createDocumentFragment();
 
 offerPopUp(fragment);
 offerList.appendChild(fragment.firstChild);
+
+
+const dropDownType = document.querySelector('#type');
+const inputPrice = document.querySelector('#price');
+
+changePriceOfType(dropDownType, inputPrice);
+
+const timeIn = document.querySelector('#timein')
+const timeOut = document.querySelector('#timeout')
+
+eventBothChange (timeIn,timeOut);
--- js/util.js
+++ js/util.js
@@ -11,4 +11,9 @@ const getRandomFloatNumber = (from, to, decimals) => {
   return (from === to) ? 'Числа не должны быть одинаковыми' : (from >= to) ? 'Первое число должно быть меньше второго' : Math.round(((Math.random() * (to - from)) + from) * Math.pow(10, decimals)) / Math.pow(10, decimals);
 };
 
-export { getRandomFloatNumber, getRandomNumber, getRandomForReduce };
+const dropDownChange = (inputElement, value) => {
+    inputElement.min=value;
+    inputElement.placeholder=value;
+  };
+
+export { getRandomFloatNumber, getRandomNumber, getRandomForReduce, dropDownChange};
